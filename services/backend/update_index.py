71f6934f36fb26a59fc8b7720af873b3e8350dee
mrgrassho <mr.grassho@gmail.com>
2020-10-09 14:54:18 -0300

    Minor fixes in BE

--- services/backend/update_index.py
+++ services/backend/update_index.py
@@ -5,11 +5,13 @@ from os import listdir, environ
 from os.path import join, dirname, isfile
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from time import sleep
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 client = MongoClient(environ.get("MONGO_URI"))
 DIR_TILES = environ.get("DIR_TILES")
+UPDATE_WAIT = int(environ.get("UPDATE_WAIT"))
 
 def remove_ds_store(ls):
     return [ f for f in ls if not f.endswith('.DS_Store')]
@@ -46,7 +48,9 @@ def update():
     db.geo_index.insert_many(get_index())
 
 def main():
-    update()
+    while True:
+        update()
+        sleep(UPDATE_WAIT)
 
 if __name__ == '__main__':
     main()
