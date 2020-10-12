8f06c3993c0669838531b5244d8193de5dde748d
mr.grassho <mr.grassho@gmail.com>
2020-10-11 22:25:40 -0300

    Fix Admin Worker (Add Keep Alive support)

--- services/admin_worker/admin_worker.py
+++ services/admin_worker/admin_worker.py
@@ -12,6 +12,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from rabbimq_client import RabbitMQClient
+from json import loads
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -47,6 +48,7 @@ class AdminWorker(object):
         self._refresh_rate = int(environ['REFRESH_RATE'])
         self._max_timeout = timedelta(seconds=int(environ['MAX_TIMEOUT']))
         self._rabbitmq_client = RabbitMQClient(
+            self,
             amqp_url = self._amqp_url,
             queues = {
                 self._task_queue: {
@@ -54,14 +56,14 @@ class AdminWorker(object):
                     "exclusive": False,
                     "auto_delete": False,
                     "auto_ack": False,
-                    "callback": self.callback_task_queue
+                    "callback": None
                 },
                 self._keep_alive_queue: {
                     "durable": True,
                     "exclusive": False,
                     "auto_delete": False,
                     "auto_ack": True,
-                    "callback": self.callback_keep_alive_queue
+                    "callback": "callback_keep_alive_queue"
                 }
             },
             reconection_time = self._reconection_time,
@@ -70,19 +72,16 @@ class AdminWorker(object):
         )
 
 
-    def callback_task_queue(self, ch, method, delivery, properties, body):
-        pass
-
-
-    def callback_keep_alive_queue(self, ch, method, delivery, properties, body):
-        data = json.loads(body)
+    def callback_keep_alive_queue(self, ch, method, properties, body):
+        data = loads(body)
         self._last_timestamps[data['id']] = data['timestamp']
-    
+
 
     def update_queue_data(self):
-        if (self._rabbitmq_client.queue_obj(self._task_queue)):
+        message_count = self._rabbitmq_client.message_count(self._task_queue)
+        if (message_count is not None):
             self._current_state['replica_count'] = self._docker_client.get_service_replica_count(service_name=self._service_monitor)
-            self._current_state['msg_count'] = self._rabbitmq_client.queue_obj(self._task_queue).method.message_count
+            self._current_state['msg_count'] = message_count
             self._current_state['load'] = self._current_state['msg_count'] / (self._current_state['replica_count'] * self._qty_task)
             self._current_state['ligth'] = self.get_ligth(self._current_state['load'])
 
@@ -132,16 +131,22 @@ class AdminWorker(object):
             if (self._debug):
                 print(f"Scaling down {self._service_monitor} from {self._current_state['replica_count']} to {scale_to} replicas")
             self._docker_client.scale_service(service_name=self._service_monitor, replica_count=scale_to)
+            sleep(1)
+            containers = self._docker_client.get_containers()
+            for container in containers:
+                if container in self._last_timestamps:
+                    self._last_timestamps.pop(container)
+
 
 
     def calculate_timeout_workers(self):
         # Calculo si algun worker lleva demasiado tiempo sin responder
         while (True):  # recorro array con workers y sus tiempos.
-            if (self._debug):
-                print(f"{BColors.GREY}\tWORKER\t\tLAST SEEN\t\tSTATUS{BColors.ENDC}")
+            if (len(self._last_timestamps) > 0 and self._debug):
+                print(f"{BColors.GREY}\tWORKER\t\tLAST SEEN\t\t\tSTATUS{BColors.ENDC}")
             removed_workers = []
-            for worker, timestamp in self._last_timestamps:
-                timeout_worker = datetime.strptime(timestamp, '%d/%m/%Y %H:%M:%S')
+            for worker, timestamp in self._last_timestamps.items():
+                timeout_worker = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
                 diff = datetime.now() - timeout_worker
                 if (diff > self._max_timeout):
                     status = ('NOT RESPONDING', BColors.RED) 
@@ -153,7 +158,7 @@ class AdminWorker(object):
                 else:
                     status = ('OK', BColors.GREEN) 
                 if (self._debug):
-                    print(f"\t{status[1]}{worker}\t\t{timestamp}\t\t{status[0]}{BColors.ENDC}")
+                    print(f"\t{status[1]}{worker}\t{timestamp}\t\t{status[0]}{BColors.ENDC}")
             for removed_worker in removed_workers:
                 if (self._debug):
                     print(f' [!] Worker {removed_worker} is NOT responding.')
@@ -162,7 +167,8 @@ class AdminWorker(object):
 
 
     def start(self):
-        self._rabbitmq_client.start()
+        t3 = Thread(target=self._rabbitmq_client.start)
+        t3.start()
         self._docker_client = DockerAPIClient()
         self._current_state = {
             'msg_count': -1,
@@ -174,7 +180,8 @@ class AdminWorker(object):
         t1.start()
         t2 = Thread(target=self.calculate_timeout_workers)
         t2.start()
-            
+
+
 def main():
     """Main entry point to the program."""
     admin = AdminWorker()
--- services/admin_worker/docker_client.py
+++ services/admin_worker/docker_client.py
@@ -13,14 +13,22 @@ class DockerAPIClient(object):
 
     def _get_service(self, service_name):
         services = self.native_docker_client.services.list(filters=dict(name=service_name))
-        return services[0]
+        return services[0] if services is not None and len(services) > 0 else None
 
 
     def get_service_replica_count(self, service_name):
         service = self._get_service(service_name)
-        return service.attrs['Spec']['Mode']['Replicated']['Replicas']
+        return service.attrs['Spec']['Mode']['Replicated']['Replicas'] if service is not None else -1
 
 
     def scale_service(self, service_name, replica_count):
         service = self._get_service(service_name)
         service.update(mode=ServiceMode("replicated", replicas=replica_count))
+
+
+    def get_containers(self, service_name):
+        containers = []
+        for container in self.native_docker_client.containers.list():
+            if container.attrs["HostConfig"]["Config"]["Labels"]["com.docker.swarm.service.name"] == service_name:
+                containers.push(container.attrs["HostConfig"]["Config"]["Hostname"])
+        return containers
